0962da91b54ca4d26cc6dacf2449d5942a11632c
kmoart <kmoart@gmail.com>
2026-05-03 16:53:28 -0500

    Faltó registar el objeto de autenticación en el contexto de seguridad

--- src/main/java/com/simonmov/MonitoreoFlotas/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/simonmov/MonitoreoFlotas/jwt/JwtAuthenticationFilter.java
@@ -49,7 +49,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             if(jwtUtil.validateToken(jwt, userDetails)){
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                         new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                usernamePasswordAuthenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
 
